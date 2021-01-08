88ccbc83b1b3943802b720747f6b2db49c88c838
JennyWauer <64482030+JennyWauer@users.noreply.github.com>
2021-01-08 11:21:24 -0600

    create form view

--- basicapp/views.py
+++ basicapp/views.py
@@ -4,3 +4,8 @@ from . import forms
 
 def index(request):
     return render(request, 'basicappindex.html')
+
+def form_name_view(request):
+    form = forms.FormName()
+    return render(request, 'basicapp/form_page.html', {'form':form})
+
